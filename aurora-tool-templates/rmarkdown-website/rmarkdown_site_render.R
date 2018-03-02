27625f4144dc3dd212caa4487d323061adca3393
Ming Chen <ming.chen0919@gmail.com>
2018-03-02 10:33:12 -0500

    update template files

--- aurora-tool-templates/rmarkdown-website/rmarkdown_site_render.R
+++ aurora-tool-templates/rmarkdown-website/rmarkdown_site_render.R
@@ -33,9 +33,9 @@ do.call(Sys.setenv, opt[-1])
 # OUTPUT_REPORT: path to galaxy output report
 # OUTPUT_DIR: path to the output associated directory, which stores all outputs
 # TOOL_DIR: path to the tool installation directory
-OUTPUT_REPORT = ''
-OUTPUT_DIR = ''
-TOOL_DIR =   ''
+OUTPUT_DIR = opt$X_d
+TOOL_DIR =   opt$X_t
+OUTPUT_REPORT = opt$X_o
 
 
 # create the output associated directory to store all outputs
@@ -43,11 +43,14 @@ dir.create(OUTPUT_DIR, recursive = TRUE)
 
 #-----------------render site--------------
 # copy site generating materials into OUTPUT_DIR
-file.copy(TOOL_DIR, OUTPUT_DIR, recursive = TRUE)
+dir.create(paste0(OUTPUT_DIR, '/site_generator'), recursive = TRUE)
+system(paste0('cp -r ', TOOL_DIR, '/TOOL_BASENAME_*.Rmd ', OUTPUT_DIR, '/site_generator/skewer.Rmd'))
+system(paste0('cp -r ', TOOL_DIR, '/TOOL_BASENAME_site.yml ', OUTPUT_DIR, '/site_generator/_site.yml'))
+system(paste0('cp -r ', TOOL_DIR, '/TOOL_BASENAME_index.Rmd ', OUTPUT_DIR, '/site_generator/index.Rmd'))
 # render site to OUTPUT_DIR/_site, this is configured in the "_site.yml" file
-render_site(input = paste0(OUTPUT_DIR, '/tool_repository_name'))
+render_site(input = paste0(OUTPUT_DIR, '/site_generator'))
 # remove site generating materials from output associated directory
-unlink(paste0(OUTPUT_DIR, '/tool_repository_name'), recursive = TRUE)
+unlink(paste0(OUTPUT_DIR, '/site_generator'), recursive = TRUE)
 # move _site/* into output associated directory
 move_cmd = paste0('mv ', OUTPUT_DIR, '/_site/* ', OUTPUT_DIR)
 system(move_cmd)
