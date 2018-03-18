6cbdb4fadaf745c70ccd764794d64a382b50713b
Ming Chen <ming.chen0919@gmail.com>
2018-03-18 11:11:36 -0400

    skewer_v1.1.0

--- aurora-galaxy-tools/aurora_skewer/skewer_v1.1.0/skewer_render.R
+++ aurora-galaxy-tools/aurora_skewer/skewer_v1.1.0/skewer_render.R
@@ -15,24 +15,13 @@ library(htmltools)
 
 
 #------------get arguments into R--------------------
-# getopt_specification_matrix(extract_short_flags('skewer.xml')) %>%
-#   write.table(file = 'spec.txt', sep = ',', row.names = FALSE, col.names = TRUE, quote = FALSE)
-
-
-spec_matrix = as.matrix(
-  data.frame(stringsAsFactors=FALSE,
-              long_flags = c("X_e", "X_o", "X_d", "X_s", "X_t", "X_X", "X_Y",
-                             "X_x", "X_y", "X_A", "X_B", "X_f", "X_r"),
-             short_flags = c("e", "o", "d", "s", "t", "X", "Y", "x", "y", "A",
-                             "B", "f", "r"),
-     argument_mask_flags = c(1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L),
-         data_type_flags = c("character", "character", "character", "character",
-                             "character", "character", "character",
-                             "character", "character", "character", "character",
-                             "character", "character")
-  )
-)
-opt = getopt(spec_matrix)
+# load helper function
+source(paste0(Sys.getenv('TOOL_DIR'), '/helper.R'))
+# import getopt specification matrix from a csv file
+spec_csv = paste0(Sys.getenv('TOOL_DIR'), '/getopt_specification.csv')
+opt = getopt(getopt_specification_matrix(spec_csv))
+opt$X_t = Sys.getenv('TOOL_DIR')
+print(opt)
 #----------------------------------------------------
 
 
@@ -46,14 +35,13 @@ do.call(Sys.setenv, opt[-1])
 # TOOL_DIR: path to the tool installation directory
 OUTPUT_DIR = opt$X_d
 TOOL_DIR =   opt$X_t
-RMD_NAME = 'skewer.Rmd'
 OUTPUT_REPORT = opt$X_o
+RMD_NAME = 'skewer.Rmd'
 
 # create the output associated directory to store all outputs
 dir.create(OUTPUT_DIR, recursive = TRUE)
-opt
+
 #-----------------render Rmd--------------
-paste0(TOOL_DIR, '/', RMD_NAME)
 render(paste0(TOOL_DIR, '/', RMD_NAME), output_file = OUTPUT_REPORT)
 #------------------------------------------
 
