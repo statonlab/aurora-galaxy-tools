4fc674b015e558efdea124301ca3eba73cee6791
Ming Chen <ming.chen0919@gmail.com>
2018-05-16 11:53:03 -0400

    update

--- single-html-tools/aurora_fasta_importer/build-and-run-job-scripts.sh
+++ single-html-tools/aurora_fasta_importer/build-and-run-job-scripts.sh
@@ -3,10 +3,22 @@ cd ${REPORT_FILES_PATH}
 
 #========== build and run job 1 script ============
 cat >curl-download.sh <<EOF
-# use the first line as an url and start downloading. If it failed, run the second command.
-curl $(head -1 $X_f) > $X_O || cp $X_f $X_O
+if [ $(wc -l <$X_f) -gt 2 ]; then
+  cp $X_f $X_O
+else
+  if [ "$X_e" = "twobit" ]; then
+    cp $TOOL_INSTALL_DIR/twoBitToFa twoBitToFa
+    chmod +x twoBitToFa
+    ./twoBitToFa $(head -1 $X_f) $X_O
+  elif [ "$X_e" = "gz" ]; then
+    curl $(head -1 $X_f) > output.fa.gz 
+    gunzip -c output.fa.gz > $X_O 
+  else
+    curl $(head -1 $X_f) > $X_O
+  fi
+fi
+
 EOF
 
-# run job 1 script
 sh curl-download.sh
 
