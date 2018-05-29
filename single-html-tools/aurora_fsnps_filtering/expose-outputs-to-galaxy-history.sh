602f5b82864867a81c2e4ea2be9b52f4b635bd7b
Ming Chen <ming.chen0919@gmail.com>
2018-05-29 13:11:19 -0400

    add new tools

---
+++ single-html-tools/aurora_fsnps_filtering/expose-outputs-to-galaxy-history.sh
@@ -0,0 +1,15 @@
+# change directory to tool outputs directory
+cd ${REPORT_FILES_PATH}
+
+# copy outputs from tool outputs directory to corresponding galaxy output path
+if [ -e "rmarkdown_report.html" ]; then
+  cp rmarkdown_report.html ${REPORT}
+fi
+
+if [ -e "index.html" ]; then
+  cp index.html ${REPORT}
+fi
+
+if [ -e "fSNPS.csv" ]; then
+  cp fSNPS.csv ${X_O}
+fi
