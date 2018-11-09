0ab80de699747ead439fcaccfdd73f66066affc1
Ming Chen <ming.chen0919@gmail.com>
2018-11-09 12:40:35 -0500

    update

---
+++ single-html-tools/aurora_findssrs/galaxy_outputs.sh
@@ -0,0 +1,16 @@
+# change directory to tool outputs directory
+cd ${REPORT_FILES_PATH}
+
+# copy outputs from tool outputs directory to corresponding galaxy output path
+if [ -e "rmarkdown_report.html" ]; then
+  cp rmarkdown_report.html ${REPORT}
+fi
+
+if [ -e "index.html" ]; then
+  cp index.html ${REPORT}
+fi
+
+
+if [ -e `*.ssr.fasta` ]; then
+  cp `*.ssr.fasta` ${X_F}
+fi
