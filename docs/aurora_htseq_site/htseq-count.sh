9d80f8019f1eb21b0bc6fdb4e3ae7684174f6ca4
Ming Chen <ming.chen0919@gmail.com>
2018-11-30 01:16:41 -0500

    update report from aurora_htseq_site

--- docs/aurora_htseq_site/htseq-count.sh
+++ docs/aurora_htseq_site/htseq-count.sh
@@ -1,12 +1,12 @@
  htseq-count \
-    /export/galaxy-central/database/files/000/dataset_34.dat /export/galaxy-central/database/files/000/dataset_35.dat \
-    /export/galaxy-central/database/files/000/dataset_36.dat \
+    /export/galaxy-central/database/files/000/dataset_41.dat /export/galaxy-central/database/files/000/dataset_42.dat /export/galaxy-central/database/files/000/dataset_43.dat /export/galaxy-central/database/files/000/dataset_44.dat \
+    /export/galaxy-central/database/files/000/dataset_45.dat \
     -f bam \
     -r name \
     -s yes \
     -a 10 \
     -t exon \
-    -i gene_id \
+    -i ID \
     -m union > htseq-counts-raw.txt
 
   grep -v '__no_feature\|__ambiguous\|__too_low_aQual\|__not_aligned\|__alignment_not_unique' htseq-counts-raw.txt > counts.txt
