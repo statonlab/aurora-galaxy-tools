036800e7aaf288637be4cefa799affdd5c383855
Ming Chen <ming@cryoocyte.com>
2018-12-05 12:38:34 -0500

    update

--- docs/aurora_fastqc/job-1-script.sh
+++ docs/aurora_fastqc/job-1-script.sh
@@ -1,12 +1,12 @@
 
-cp /export/galaxy-central/database/files/000/dataset_1.dat read_1.fq
-cp /export/galaxy-central/database/files/000/dataset_5.dat read_2.fq
+cp /export/galaxy-central/database/files/000/dataset_6.dat read_1.fq
+cp /export/galaxy-central/database/files/000/dataset_14.dat read_2.fq
 
 fastqc \
   -q \
-  /export/galaxy-central/database/job_working_directory/000/3/dataset_6_files/read_1.fq > /dev/null 2>&1
+  /export/galaxy-central/database/job_working_directory/000/42/dataset_49_files/read_1.fq > /dev/null 2>&1
   
 fastqc \
   -q \
-  /export/galaxy-central/database/job_working_directory/000/3/dataset_6_files/read_2.fq > /dev/null 2>&1
+  /export/galaxy-central/database/job_working_directory/000/42/dataset_49_files/read_2.fq > /dev/null 2>&1
 
